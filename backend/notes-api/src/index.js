6d591bf0bb66fcdc365a58d4d1f2b39e6433c28d
Hamza <hamzahalam2001@proton.me>
2025-11-26 19:28:12 +0500

    created dummy notes.json and returning notes in response from server

--- backend/notes-api/src/index.js
+++ backend/notes-api/src/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
+
+app.get('/notes', (req, res) => {
+    fs.readFile(__dirname + '/' + 'notes.json', 'utf-8', (err, data) => {
+        if (err) {
+            return console.log(err)
+        }
+        res.status(200).send(data)
+    })
+})
 
 app.listen(3000, () => {
     console.log("server is up running on port 3000")
