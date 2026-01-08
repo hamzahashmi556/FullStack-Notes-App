adb3edf7a66ec8962dc0a3d6f1c8b1efb5eb50d0
Hamza <hamzahalam2001@proton.me>
2026-01-08 13:50:45 +0500

    FrontEnd Project Inititialized in Xcode

---
+++ frontend/Notes/Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Hamza Hashmi on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
